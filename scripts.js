a4a622b3551a4d49ba61ecda1e091c458fa7bc5f
Anuj Hoslok <anujhoslokdev@gmail.com>
2022-08-07 22:08:45 +0530

    changed font and and the look of the form, project finished for now

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 
-const range = document.querySelector('#howmany');
-const output = document.querySelector('#result');
+// const range = document.querySelector('#howmany');
+// const output = document.querySelector('#result');
+const password1 = document.querySelector('#pass');
+const password2 = document.querySelector('#pass1');
 
+// output.textContent = range.value;
 
-output.textContent = range.value;
+// range.oninput = function()
+// {
+//     output.textContent = this.value;
+// }
 
-range.oninput = function()
-{
-    output.textContent = this.value;
-}
+// if(password1!=password2)
+// {
+//     document.getElementById('pass').style.cssText = "border:3px solid red;";
+//     document.getElementById('pass1').style.cssText = "border:3px solid red;";
+// }
